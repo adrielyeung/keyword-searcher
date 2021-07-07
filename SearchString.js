41461a2d0690bc561d48aa0e2bc235284ab3506b
adrielyeung <adrielyeung1997@gmail.com>
2021-07-07 16:37:39 +0800

    Refactored code to comply with JSLint standard

--- SearchString.js
+++ SearchString.js
@@ -1,24 +1,26 @@
 // Load a text from disk
 function loadText(filename, displayName) {
-    resetUI(displayName);
-
     // Load stop word list from config
     // Create a HTTP server request to load text
     // When text are uploaded, it is hosted on the server
     var xhrStopWord = new XMLHttpRequest();
-
     // Load stop word list from config
-    let stopWordArray = [];
-    let stopWordUrl = "config/stopwords.txt";
+    var stopWordArray = [];
+    var stopWordUrl = "config/stopwords.txt";
 
-    // Initialises the connection, asynchronous request meaning screen is not frozen while execution
+    resetUI(displayName);
+
+    // Initialises the connection, asynchronous request
+    // meaning screen is not frozen while execution
     xhrStopWord.open("GET", stopWordUrl, true);
     xhrStopWord.send();
 
-    // On ready state change event, calling anonymous function every time the request state changes
+    // On ready state change event, calling anonymous function
+    // every time the request state changes
     xhrStopWord.onreadystatechange = function () {
-        // Ready state 0 = unsent, 1 = opened, 2 = header received, 3 = loading, 4 = done
-        if (xhrStopWord.readyState == 4 && xhrStopWord.status == 200) {
+        // Ready state 0 = unsent, 1 = opened,
+        // 2 = header received, 3 = loading, 4 = done
+        if (xhrStopWord.readyState === 4 && xhrStopWord.status === 200) {
             // Load response text as current text
             stopWordArray = xhrStopWord.responseText.split(/\s+/);
         }
@@ -26,32 +28,38 @@ function loadText(filename, displayName) {
 
     setTimeout(function () {
         // Load text
-        let currentText = "";
-        let url = "text/" + filename;
+        var currentText = "";
+        var url = "text/" + filename;
 
         // Create a HTTP server request to load text
         // When text are uploaded, it is hosted on the server
         var xhr = new XMLHttpRequest();
-        // Initialises the connection, asynchronous request meaning screen is not frozen while execution
+        // Initialises the connection, asynchronous request
+        // meaning screen is not frozen while execution
         xhr.open("GET", url, true);
         xhr.send();
 
-        // On ready state change event, calling anonymous function every time the request state changes
+        // On ready state change event, calling anonymous function
+        // every time the request state changes
         xhr.onreadystatechange = function () {
-            // Ready state 0 = unsent, 1 = opened, 2 = header received, 3 = loading, 4 = done
-            if (xhr.readyState == 4 && xhr.status == 200) {
+            // Ready state 0 = unsent, 1 = opened,
+            // 2 = header received, 3 = loading, 4 = done
+            if (xhr.readyState === 4 && xhr.status === 200) {
                 // Load response text as current text
-                // FORMATTING: Remove line breaks and carriage returns and replace with HTML <br> tag
-                // ?: defines a non-capturing group, i.e. the LB and CR chars are not separately returned in a list
+                // FORMATTING: Remove line breaks and carriage returns
+                // and replace with HTML <br> tag
+                // ?: defines a non-capturing group, i.e. the LB and CR chars
+                // are not separately returned in a list
                 currentText = xhr.responseText;
 
                 if (stopWordArray.length === 0) {
-                    alert("Stop word list cannot be loaded. The most used words list will be affected.");
+                    alert("Stop word list cannot be loaded. " +
+                          "The most used words list will be affected.");
                 }
 
                 getDocStats(currentText, stopWordArray);
 
-                currentText = currentText.replace(/(?:\r\n|\r|\n)/g, '<br>');
+                currentText = currentText.replace(/(?:\r\n|\r|\n)/g, "<br>");
 
                 document.getElementById("fileContent").innerHTML = currentText;
 
@@ -61,60 +69,66 @@ function loadText(filename, displayName) {
         };
 
         setTimeout(setTextNotFound(), 1000);
-    }, 1000);   
+    }, 1000);
 }
 
 // Load text from user input
 function loadTextFromInput() {
+    // Load stop word list from config
+    // Create a HTTP server request to load text
+    // When text are uploaded, it is hosted on the server
+    var xhrStopWord = new XMLHttpRequest();
+
+    // Load stop word list from config
+    var stopWordArray = [];
+    var stopWordUrl = "config/stopwords.txt";
+
     if (document.getElementById("userContent").value.trim().length > 0) {
         if (document.getElementById("userTitle").value.trim().length === 0) {
             resetUI("Your text");
         } else {
             resetUI(document.getElementById("userTitle").value);
         }
-        
-        // Load stop word list from config
-        // Create a HTTP server request to load text
-        // When text are uploaded, it is hosted on the server
-        var xhrStopWord = new XMLHttpRequest();
-    
-        // Load stop word list from config
-        let stopWordArray = [];
-        let stopWordUrl = "config/stopwords.txt";
-    
-        // Initialises the connection, asynchronous request meaning screen is not frozen while execution
+
+        // Initialises the connection, asynchronous request
+        // meaning screen is not frozen while execution
         xhrStopWord.open("GET", stopWordUrl, true);
         xhrStopWord.send();
-    
-        // On ready state change event, calling anonymous function every time the request state changes
+
+        // On ready state change event, calling anonymous function
+        // every time the request state changes
         xhrStopWord.onreadystatechange = function () {
-            // Ready state 0 = unsent, 1 = opened, 2 = header received, 3 = loading, 4 = done
-            if (xhrStopWord.readyState == 4 && xhrStopWord.status == 200) {
+            // Ready state 0 = unsent, 1 = opened,
+            // 2 = header received, 3 = loading, 4 = done
+            if (xhrStopWord.readyState === 4 && xhrStopWord.status === 200) {
                 // Load response text as current text
                 stopWordArray = xhrStopWord.responseText.split(/\s+/);
             }
         };
-    
+
         // Timeout - wait for stop word to populate before loading text
         setTimeout(function () {
             // Load text from textarea
-            // FORMATTING: Remove line breaks and carriage returns and replace with HTML <br> tag
-            // ?: defines a non-capturing group, i.e. the LB and CR chars are not separately returned in a list
-            let currentText = document.getElementById("userContent").value;
-    
+            // FORMATTING: Remove line breaks and carriage returns
+            // and replace with HTML <br> tag
+            // ?: defines a non-capturing group, i.e. the LB and CR chars
+            // are not separately returned in a list
+            var currentText = document.getElementById("userContent").value;
+
             if (stopWordArray.length === 0) {
-                alert("Stop word list cannot be loaded. The most used words list will be affected.");
+                alert("Stop word list cannot be loaded. " +
+                "The most used words list will be affected.");
             }
-    
+
             getDocStats(currentText, stopWordArray);
-    
-            currentText = currentText.replace(/(?:\r\n|\r|\n)/g, '<br>');
-    
+
+            currentText = currentText.replace(/(?:\r\n|\r|\n)/g, "<br>");
+
             document.getElementById("fileContent").innerHTML = currentText;
-    
+
             // Scroll to top
             document.getElementById("fileContent").scrollTop = 0;
-    
+
             setTimeout(setTextNotFound(), 1000);
         }, 1000);
     }
@@ -132,7 +146,8 @@ function resetUI(displayName) {
 }
 
 function setTextNotFound() {
-    if (document.getElementById("fileContent").innerHTML === "Loading..." || document.getElementById("fileContent").innerHTML.length === 0) {
+    if (document.getElementById("fileContent").innerHTML === "Loading..." ||
+        document.getElementById("fileContent").innerHTML.length === 0) {
         document.getElementById("fileContent").innerHTML = "Text not found";
     }
 }
@@ -142,60 +157,65 @@ function getDocStats(fileContent, stopWordArray) {
     var docLength = document.getElementById("docLength");
     var wordCount = document.getElementById("wordCount");
 
-    let text = fileContent.toLowerCase();
+    var text = fileContent.toLowerCase();
     // Matches any chars between spaces (a word)
-    let wordArray = text.match(/\b\S+\b/g);
+    var wordArray = text.match(/\b\S+\b/g);
+    var textWithoutLineBreaks = text.replace(/(?:\r\n|\r|\n)/g, "");
 
     // JavaScript object
-    let wordDictionary = {};
+    var wordDictionary = {};
 
     // Filter out stop words
     var useWordArray = filterStopWords(wordArray, stopWordArray);
-    
-    // Count words in useWordArray (filtered)
-    for (let word in useWordArray) {
-        let wordValue = useWordArray[word];
 
+    var wordList;
+    var top5Words;
+    var least5Words;
+
+    // Count words in useWordArray (filtered)
+    useWordArray.forEach(function (wordValue) {
         if (wordDictionary[wordValue] > 0) {
             wordDictionary[wordValue] += 1;
         } else {
             wordDictionary[wordValue] = 1;
         }
-    }
+    });
 
     // Sort the array
-    let wordList = sortProperties(wordDictionary);
+    wordList = sortProperties(wordDictionary);
 
     // Return top 5 words
-    var top5Words = wordList.slice(0, 5);
+    top5Words = wordList.slice(0, 5);
     // Return least used 5 words
-    var least5Words = wordList.slice(-5);
+    least5Words = wordList.slice(-5);
 
     // Write the values to the page
     ULTemplate(top5Words, document.getElementById("mostUsed"));
     ULTemplate(least5Words, document.getElementById("leastUsed"));
 
     // Get document stats
-    var textWithoutLineBreaks = text.replace(/(?:\r\n|\r|\n)/g, '');
-    docLength.innerText = "Document Length : " + textWithoutLineBreaks.length + " character(s)";
-    wordCount.innerText = "Word Count : " + wordArray.length + " word(s)";
+    docLength.innerText = "Document Length : " + textWithoutLineBreaks.length
+                        + " character(s)";
+    wordCount.innerText = "Word Count : " + wordArray.length
+                        + " word(s)";
 }
 
 function ULTemplate(items, element) {
-    let rowTemplate = document.getElementById('template-ul-items');
-    let templateHTML = rowTemplate.innerHTML;
-    let resultsHTML = "";
-     
-    for (let i = 0; i < items.length; i++) {
-        resultsHTML += templateHTML.replace('{{val}}', items[i][0] + " : " + items[i][1] + " time(s)");
-    }
+    var rowTemplate = document.getElementById("template-ul-items");
+    var templateHTML = rowTemplate.innerHTML;
+    var resultsHTML = "";
+
+    items.forEach((item) =>
+        resultsHTML += templateHTML.replace("{{val}}", item[0] + " : "
+                    + item[1] + " time(s)")
+    );
 
     element.innerHTML = resultsHTML;
 }
 
 function sortProperties(object) {
     // First convert object to array
-    let rtnArray = Object.entries(object);
+    var rtnArray = Object.entries(object);
 
     // Sort the array based on comparer function
     rtnArray.sort(function (first, second) {
@@ -210,17 +230,17 @@ function filterStopWords(wordArray, stopWordArray) {
     var outputArray = [];
 
     // Load stop words into object
-    for (let i = 0; i < stopWordArray.length; i++) {
-        stopObject[stopWordArray[i].trim().toLowerCase()] = true;
-    }
+    stopWordArray.forEach((stopWord) =>
+        stopObject[stopWord.trim().toLowerCase()] = true
+    );
 
-    // Add words which are not 'stopped' into output
-    for (let i = 0; i < wordArray.length; i++) {
-        word = wordArray[i].trim().toLowerCase();
-        if (!stopObject[word]) {
-            outputArray.push(word);
+    // Add words which are not "stopped" into output
+    wordArray.forEach(function (word) {
+            if (!stopObject[word.trim().toLowerCase()]) {
+                outputArray.push(word.trim().toLowerCase());
+            }
         }
-    }
+    );
 
     return outputArray;
 }
@@ -229,32 +249,40 @@ function filterStopWords(wordArray, stopWordArray) {
 function performHighlight() {
     // Read keyword
     var keyword = document.getElementById("keyword").value;
-    if (keyword.length > 0) {
-        var display = document.getElementById("fileContent");
+    var display;
+    var re;
+    var replaceText;
+    var bookContent;
+    var newContent;
+    var count;
 
-        var newContent = "";
+    if (keyword.length > 0) {
+        display = document.getElementById("fileContent");
+
+        newContent = "";
 
         resetHighlight();
 
         // Auto generate regexp with option "gi" - global and insensitive
-        var re = new RegExp(keyword, "gi");
+        re = new RegExp(keyword, "gi");
         // String format into $&
-        var replaceText = "<mark id='markme'>$&</mark>";
-        var bookContent = display.innerHTML;
+        replaceText = "<mark id='markme'>$&</mark>";
+        bookContent = display.innerHTML;
 
         // Highlight the elements
-        var newContent = bookContent.replace(re, replaceText);
+        newContent = bookContent.replace(re, replaceText);
+
 
         // Display new content
         display.innerHTML = newContent;
         // Count number of highlighted
-        var count = document.querySelectorAll('mark').length;
-        document.getElementById("searchStat").innerHTML = "Search for : " + keyword + "<br>Found matches : " + count + " time(s)";
+        count = document.querySelectorAll("mark").length;
+        document.getElementById("searchStat").innerHTML = "Search for : "
+            + keyword + "<br>Found matches : " + count + " time(s)";
 
         // Scroll to first match if there are matches
         if (count > 0) {
-            var element = document.getElementById("markme");
-            element.scrollIntoView();
+            document.getElementById("markme").scrollIntoView();
         }
     }
 }
@@ -262,14 +290,14 @@ function performHighlight() {
 // Reset highlighted
 function resetHighlight() {
     // Find all currently highlighted items
-    // Look for tags named 'mark', <mark></mark>
-    let spans = document.querySelectorAll('mark');
+    // Look for tags named "mark", <mark></mark>
+    var spans = document.querySelectorAll("mark");
 
     // Remove highlight mark for all currently highlighted
-    // <mark id='highlight>abc</mark> -> abc
-    for (let i = 0; i < spans.length; i++) {
-        spans[i].outerHTML = spans[i].innerHTML;
-    }
+    // <mark id='highlight'>abc</mark> -> abc
+    spans.forEach((span) =>
+        span.outerHTML = span.innerHTML
+    );
 
     document.getElementById("searchStat").innerHTML = "";
 }
